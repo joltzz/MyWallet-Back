33752a59d546d8b89f4109bb1ff40bd3d58deac6
joltzz <guijsilva02@gmail.com>
2022-05-06 10:47:42 -0300

    extract e user controller done

---
+++ src/controllers/extractController.js
@@ -0,0 +1,13 @@
+import db from "../db";
+
+
+export async function getExtract(request, response){
+    const user=response.locals.user;
+
+    try {
+        const entries = await db.collection('entries').find({ userId: user._id }).toArray();
+        response.send({ ...user, entries });
+    } catch (error) {
+        response.status(500).send(error);
+    }
+}
