2430d7d77546b9c31161e1d72bfdde738e256bf0
joltzz <guijsilva02@gmail.com>
2022-05-09 10:18:12 -0300

    routers finalizados

--- src/app.js
+++ src/app.js
@@ -1,18 +1,15 @@
-import express,{json} from "express";
-import cors from "cors";
-import chalk from "chalk";
+import express, { json } from 'express';
+import cors from 'cors';
 
-// import
+import router from './routes/index.js';
 
-const app=express();
+const app = express();
 
-app.use(json());
 app.use(cors());
+app.use(json());
 
+app.use(router);
 
-
-app.listen(process.env.PORT, ()=>{
-    console.log(chalk.bold.green(`Server running on port: ${process.env.PORT}`));
-})
-
-
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT);
+});
--- src/db.js
+++ src/db.js
@@ -7,7 +7,7 @@ const mongoClient= new MongoClient(process.env.MONGO_URI);
 
 await mongoClient.connect();
 
-const db=mongoClient.db("mywallet-back");
+const db=mongoClient.db("mywallet-backend");
 
 export default db;
 
